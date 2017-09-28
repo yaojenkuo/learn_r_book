aba5e1f2e317d3817311f3498d5cde45f9b91448
yaojenkuo <tonykuoyj@gmail.com>
2017-09-28 15:57:59 +0800

    added disqus feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+    class CustomRender < Redcarpet::Render::HTML
+        def image(link, title, alt_text)
+            if link =~ /^(.+?)\s*=+(\d+)(?:px|)$/
+            # e.g. ![alt](url.png =100px)
+            # e.g. ![alt](url.png =100)
+                %(<img src="#{$1}" style="max-width: #{$2}px" alt="#{alt_text}">)
+            elsif link =~ /^(.+?)\s*=+(\d+)(?:px|)x(\d+)(?:px|)$/
+            # e.g. ![alt](url.png =30x50)
+                %(<img src="#{$1}" style="max-width: #{$2}px; max-height: #{$3}px;" alt="#{alt_text}">)
+            else
+                %(<img src="#{link}" title="#{title}" alt="#{alt_text}">)
+            end
+        end
+    end
     class HTMLwithPygments < Redcarpet::Render::HTML
         def block_code(code, language)
             Pygments.highlight(code, lexer: language)
